761c99401709263a4d9702ffb5cdfae5510b2eff
ADMIN <ADMIN@DESKTOP-M57JVOT>
2025-08-02 20:59:05 +0530

    Added basic Project template with dependencies

---
+++ src/main/java/com/northwindspringbootapi/NorthwindspringbootapiApplication.java
@@ -0,0 +1,13 @@
+package com.northwindspringbootapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NorthwindspringbootapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NorthwindspringbootapiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/northwindspringbootapi/NorthwindspringbootapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.northwindspringbootapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NorthwindspringbootapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
