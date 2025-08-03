b01503ca13a999aa7dfaa0d25439f1c92bc100f8
ADMIN <ADMIN@DESKTOP-M57JVOT>
2025-08-03 20:52:55 +0530

    Code Update on JWT Auth API Service

--- src/main/java/com/northwindspringbootapi/controllers/v1/AuthController.java
+++ src/main/java/com/northwindspringbootapi/controllers/v1/AuthController.java
@@ -6,16 +6,27 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.HashMap;
+import java.util.Map;
+
 @RestController
-@RequestMapping("/auth")
+@CrossOrigin(origins = "http://localhost:4200")
+//@RequestMapping("/auth")
 @RequiredArgsConstructor
 public class AuthController {
 
     private final AuthService authService;
 
-    @PostMapping("/login")
-    public ResponseEntity<String> login(@RequestBody LoginRequest loginRequest) {
-        String token = authService.login(loginRequest.getUsername(), loginRequest.getPassword());
-        return ResponseEntity.ok(token);
+    @PostMapping("/auth/login")
+    public ResponseEntity<Map<String, String>> login(@RequestBody LoginRequest loginRequest) {
+//    public ResponseEntity<String> login(@RequestBody LoginRequest loginRequest) {
+    //public ResponseEntity<String> login(String username, String password) {
+        //String token = authService.login(username, password);
+         String token = authService.login(loginRequest.getUsername(), loginRequest.getPassword());
+        Map<String, String> response = new HashMap<>();
+        response.put("message", "Login successful");
+        response.put("token", token);
+        //return ResponseEntity.ok(token);
+        return ResponseEntity.ok(response);
     }
 }
